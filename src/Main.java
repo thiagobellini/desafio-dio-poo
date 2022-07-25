85637ec2c761ca6ec3ed9624172b0b265fb476b2
Thiago Bellini <thiagobellinics@hotmail.com>
2022-07-24 22:29:38 -0300

    Finalização do código

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.Conteudo;
-import br.com.dio.desafio.Curso;
-import br.com.dio.desafio.Mentoria;
+import br.com.dio.desafio.*;
 
 import java.time.LocalDate;
 
@@ -21,8 +19,40 @@ public class Main {
         mentoria.setDescricao("Descricao mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devThiago = new Dev();
+        devThiago.setNome("Thiago");
+        devThiago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Thiago: " + devThiago.getConteudosInscritos());
+        devThiago.progredir();
+        devThiago.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Thiago: " + devThiago.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Thiago: " + devThiago.getConteudosConcluidos());
+        System.out.println("XP: " + devThiago.calcularXp());
+
+        System.out.println("-------------------------------------");
+
+        Dev devClaudiane = new Dev();
+        devClaudiane.setNome("Claudiane");
+        devClaudiane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Claudiane: " + devClaudiane.getConteudosInscritos());
+        devClaudiane.progredir();
+        devClaudiane.progredir();
+        devClaudiane.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Claudiane: " + devClaudiane.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Claudiane: " + devClaudiane.getConteudosConcluidos());
+        System.out.println("XP" + devClaudiane.calcularXp());
     }
 }
